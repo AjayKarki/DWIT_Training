301599d171212c9b2a267001b380f669beb17c04
AjayKarki <ajaykarki333@gmail.com>
2019-12-03 08:56:43 +0545

    Day2

--- Day1/FirstAssignment.py
+++ Day1/FirstAssignment.py
@@ -1,6 +1,11 @@
 
 first_number = int(input("Enter a number "))
 second_number = int(input("Enter another number "))
+"""
+This is an example of
+Multiline
+Comment
+"""
 print("Sum is ", first_number+second_number)
 print("Diff is ", first_number-second_number)
 print("Prod is ", first_number*second_number)
---
+++ Day2/Day2.py
@@ -0,0 +1,21 @@
+
+# Every variable is an object of a class
+# complex_no = 3j
+# print(type(complex_no))
+
+"""
+Collections
+"""
+# List
+# list_number = [1, 2, 3, 4, 5]
+# list_student =["Ram", "Shyam", "Hari"]
+# print(type(list_student))
+# print(list_student)
+# print((list_student[1]))
+
+# list_alphabet = ['a', 'b', 'c', 'd']
+# # " start:end:step
+# print(list_alphabet[0:2:1])
+
+list_number = [1, 2, 3, 4, 5, 6, 7]
+print(list_number[1:6:2])
