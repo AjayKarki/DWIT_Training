fcd06c329891ff3fb9b04cce2dd05fb09ea2c7d4
AjayKarki <ajaykarki333@gmail.com>
2019-12-03 07:36:03 +0545

    1st

---
+++ Day1/FirstAssignment.py
@@ -0,0 +1,8 @@
+
+first_number = int(input("Enter a number "))
+second_number = int(input("Enter another number "))
+print("Sum is ", first_number+second_number)
+print("Diff is ", first_number-second_number)
+print("Prod is ", first_number*second_number)
+print("Div is ", first_number/second_number)
+print("Mod is ", first_number % second_number)
---
+++ Day1/first.py
@@ -0,0 +1,28 @@
+# num1 = 5
+# num12 = 2
+# # sum = num1+num12
+#
+# print("Sum is ", num1+num12)
+# print("Diff is ", num1-num12)
+# print("Prod is ", num1*num12)
+# print("Div is ", num1/num12)
+# print("Mod is ", num1%num12)
+#
+# first_name = "Lucifer"
+# print(first_name)
+
+# first_number = int(input("Enter a number "))
+# second_number = int(input("Enter another number "))
+# print(first_number+second_number)
+# test = input()
+# print(type(test))
+
+# Assignment Input a number Do all !!!
+
+first_number = int(input("Enter a number "))
+second_number = int(input("Enter another number "))
+print("Sum is ", first_number+second_number)
+print("Diff is ", first_number-second_number)
+print("Prod is ", first_number*second_number)
+print("Div is ", first_number/second_number)
+print("Mod is ", first_number%second_number)
