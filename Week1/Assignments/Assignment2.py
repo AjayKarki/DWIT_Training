61dff077f58e426e3fd45f103230f6a101058908
AjayKarki <ajaykarki333@gmail.com>
2019-12-08 17:01:38 +0545

    Minor Changes

--- Week1/Assignments/Assignment2.py
+++ Week1/Assignments/Assignment2.py
@@ -16,4 +16,4 @@ even_count = odd_count = 0
 for number in numbers:
     if number % 2 == 0:
         even_count += 1
-print("No. of Even numbers ",even_count,"\nNo. of Odd numbers ", len(numbers)-even_count)
+print("No. of Even numbers ", even_count, "\nNo. of Odd numbers ", len(numbers)-even_count)
--- Week1/Assignments/Assignment3.py
+++ Week1/Assignments/Assignment3.py
@@ -1,6 +1,6 @@
 number = int(input("Enter a number"))
 is_prime = 1
-for n in range(2, number//2 + 1):    # not necessary to goto the number reaching upto half of the number is fine
+for n in range(2, number//2):    # not necessary to goto the number reaching upto half of the number is fine
     if number % n == 0:
         is_prime = 0
         break
