a9630a2f6a61e764147c6715f850c730cdf38d0f
AjayKarki <ajaykarki333@gmail.com>
2019-12-19 08:55:29 +0545

    Operations Modified

--- Week3/Day13/Task_Operations.py
+++ Week3/Day13/Task_Operations.py
@@ -1,9 +1,9 @@
 class Operations:
-    def __init__(self, num1, num2):
+    def __init__(self, num1: int, num2: int):  # to indicate that num1,num2 should be strictly int
         self.num1 = num1
         self.num2 = num2
 
-    def add(self):
+    def add(self) -> int:  # to indicate that add strictly returns int
         return self.num1 + self.num2
 
     def sub(self):
@@ -16,7 +16,7 @@ class Operations:
         return self.num1 / self.num2
 
 
-c = Operations(5, 4)
+c = Operations(5, 2)
 print(c.add())
 print(c.sub())
 print(c.mul())
