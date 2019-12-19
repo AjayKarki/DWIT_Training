1bba7fa304f932138e9f89371d22e17808f53eb3
AjayKarki <ajaykarki333@gmail.com>
2019-12-19 08:52:23 +0545

    Operations

---
+++ Week3/Day13/Task_Operations.py
@@ -0,0 +1,23 @@
+class Operations:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def add(self):
+        return self.num1 + self.num2
+
+    def sub(self):
+        return self.num1 - self.num2
+
+    def mul(self):
+        return self.num1 * self.num2
+
+    def div(self):
+        return self.num1 / self.num2
+
+
+c = Operations(5, 4)
+print(c.add())
+print(c.sub())
+print(c.mul())
+print(c.div())
